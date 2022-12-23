059aa58250b0e6b4fbdf3b26b418a962c9c25f97
Divya Sri Emandi <117112551+divyasriemandi@users.noreply.github.com>
2022-12-22 18:14:30 -0600

    committing final sqls

---
+++ week5/monday/sql-basics/solutions/person.sql
@@ -0,0 +1,59 @@
+/* #1 */
+CREATE TABLE person ( 
+    person_id SERIAL PRIMARY KEY, name VARCHAR(200),
+     age INTEGER, height INTEGER, city VARCHAR(200), 
+     favorite_color VARCHAR(200));
+
+/* #2 */
+INSERT INTO person ( name, age, height, city, favorite_color ) 
+VALUES ( 'xyz vyt', 21, 182, 'City', 'Black' );
+VALUES ( 'First vgv', 21, 182, 'Dallas', 'Orange' );
+VALUES ( 'First yhgy', 28, 192, 'Atlanta', 'Yellow' );
+VALUES ( 'First bhbh', 29, 182, 'Frisco', 'White' );
+VALUES ( 'First Last', 24, 190, 'McKinney', 'White' );
+VALUES ( 'First Last', 27, 182, 'Plano', 'Black' );
+VALUES ( 'First Last', 24, 162, 'Orlando', 'Purple' );
+VALUES ( 'First Last', 27, 152, 'Vegas', 'Black' );
+VALUES ( 'First Last', 25, 202, 'Hyderabad', 'White' );
+
+/* #3 */
+SELECT * FROM person
+ ORDER BY height DESC;
+
+/* #4 */
+SELECT * FROM person
+ ORDER BY height ASC;
+
+/* #5 */
+SELECT * FROM person 
+ORDER BY age DESC;
+
+/* #6 */
+SELECT * FROM person 
+WHERE age > 20;
+
+/* #7 */
+SELECT * FROM person 
+WHERE age = 18;
+
+/* #8 */
+SELECT * FROM person 
+WHERE age < 20 OR age > 30;
+
+/* #9 */
+SELECT * FROM person WHERE age != 27;
+
+/* #10 */
+SELECT * FROM person WHERE favorite_color != 'red';
+
+/* #11 */
+SELECT * FROM person WHERE favorite_color != 'red' AND favorite_color != 'blue';
+
+/* #12 */
+SELECT * FROM person WHERE favorite_color = 'orange' OR favorite_color = 'green';
+
+/* #13 */
+SELECT * FROM person WHERE favorite_color IN ( 'orange', 'green', 'blue' );
+
+/* #14 */
+SELECT * FROM person WHERE favorite_color IN ( 'yellow', 'purple' )
